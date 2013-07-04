6bf0ba2f81448f2fbf29280c48ec041d437da7c0
phi <phi1618jp@gmail.com>
2013-07-05 02:21:41 +0900

    fix

--- src/app/sprite.jsx
+++ src/app/sprite.jsx
@@ -8,10 +8,10 @@ import "canvaselement.jsx";
  * Scene class
  */
 class Sprite extends CanvasElement {
-	var image: HTMLImageElement;
-	var width  = 32;
-	var height = 32;
-	var loaded = false;
+    var image: HTMLImageElement;
+    var width  = 32;
+    var height = 32;
+    var loaded = false;
 
     /**
      * constructor
@@ -30,19 +30,19 @@ class Sprite extends CanvasElement {
         this.image.src = src;
         var self = this;
         this.image.addEventListener("load", function(e:Event): void {
-        	self.loaded = true;
-        	self.width = self.image.width;
-        	self.height= self.image.height;
+            self.loaded = true;
+            self.width = self.image.width;
+            self.height= self.image.height;
         });
     }
     
     override function draw(ctx: CanvasRenderingContext2D): void {
-    	if (this.loaded == true) {
-	    	ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
-    	}
-    	else {
-	        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-    	}
+        if (this.loaded == true) {
+            ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+        }
+        else {
+            ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        }
     }
 }
 
